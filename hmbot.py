87e77982cf51b0b5aeee7b317c7b4b721dc557af
Guan Yang <guan@yang.dk>
2017-04-04 01:25:50 +0000

    hmbot greets people

--- hmbot.py
+++ hmbot.py
@@ -1,4 +1,4 @@
-import json, re, requests, os, sys, logging, bottle
+import json, re, requests, os, sys, logging, bottle, spacy
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('hmbot')
@@ -6,6 +6,8 @@ logger.setLevel(logging.DEBUG)
 
 slack_token         = os.environ['SLACK_TOKEN']
 verification_token  = os.environ['VERIFICATION_TOKEN']
+
+nlp = spacy.load('en')
 
 def api_call(method, **kwargs):
     """
@@ -25,7 +27,7 @@ def api_call(method, **kwargs):
 
     return response
 
-def handle_message(message):
+def handle_post(message):
     if 'type' not in message:
         logger.info(f"type missing in message {message}")
         return
@@ -44,14 +46,47 @@ def handle_message(message):
         logger.debug(f"unknown message type {t} in message {message}")
 
 def handle_event(event):
-    logger.info(f"handling event {event}")
+    if 'type' not in event:
+        logger.info(f"type missing in event {event}")
+        return
+
+    t = event['type']
+
+    if t == 'message':
+        handle_message(event)
+    else:
+        logger.debug(f"unknown event type {t} in event {event}")
+
+def handle_message(event):
+    # Ignore bot messages
+    if 'bot_id' in event:
+        logger.debug(f"ignoring because this is a bot message")
+        return
+
+    if 'subtype' in event:
+        logger.debug(f"ignoring because this is a subtyped message")
+        return
+
+    logger.debug(f"received message {event}")
+
+    # If a token is hmbot, respond
+    doc = nlp(event['text'])
+
+    # Lowercase tokens
+    lowers = [str(token).lower() for token in doc]
+
+    # See if anyone said hello
+    if 'hmbot' in lowers:
+        hmbot_index = lowers.index('hmbot')
+        if hmbot_index > 0 and lowers[hmbot_index - 1] in ['hello', 'hi', 'greetings', 'howdy', '你好', 'goddag', 'hej', 'hejsa', 'hey', 'sup']:
+            api_call('chat.postMessage', channel=event['channel'], text='Hello, I am hmbot!')
 
 @bottle.post('/')
 def slack_event_api():
     # bottle is nice and simply but has a horrible design where request and
     # response are global objects.
     try:
-        return handle_message(bottle.request.json) or ''
+        return handle_post(bottle.request.json) or ''
     except:
         logger.error("Error in handle_event", exc_info=True)
         return ''
